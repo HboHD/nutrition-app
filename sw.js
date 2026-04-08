e9b35bea618e17d18fe31e1c00dd3bf369ff3e4a
HboHD <slaskiezakladyazotowe@gmail.com>
2026-04-08 20:54:28 +0200

    SW: auto-update with skipWaiting + cache versioning

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V='v1';
-self.addEventListener('install',e=>e.waitUntil(caches.open(V).then(c=>c.addAll(['./']))));
+const V='v2';
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(V).then(c=>c.addAll(['./'])))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==V).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(res=>{caches.open(V).then(c=>c.put(e.request,res.clone()));return res}).catch(()=>caches.match(e.request))));
