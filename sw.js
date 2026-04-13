8c806adc73792ec6c4e7aabe93ed0d04366d1d7f
HboHD <slaskiezakladyazotowe@gmail.com>
2026-04-13 18:14:32 +0200

    feat: 85 inspiration recipes with source filter tabs, sw v5

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V='v4';
+const V='v5';
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(V).then(c=>c.addAll(['./'])))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==V).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(res=>{caches.open(V).then(c=>c.put(e.request,res.clone()));return res}).catch(()=>caches.match(e.request))));
